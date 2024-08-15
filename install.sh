759b12b5973eb9f0d1e4dc16c9b281a3c56a479c
AlvinNorin <alvin@norin.family>
2024-08-15 22:43:20 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+echo "Downloading- and installing script- and service ..."
+
+wget https://raw.githubusercontent.com/AlvinNorin/keep-buff-cache-cleared/main/clear_cache.sh -P /tmp/
+wget https://raw.githubusercontent.com/AlvinNorin/keep-buff-cache-cleared/main/clear_cache.service -P /tmp/
+
+sudo mv /tmp/clear_cache.sh /usr/local/bin/clear_cache.sh
+sudo mv /tmp/clear_cache.service /etc/systemd/system/clear_cache.service
+
+sudo chmod +x /usr/local/bin/clear_cache.sh
+
+echo "Enabling- and starting service ..."
+
+sudo systemctl enable clear_cache.service
+sudo systemctl start clear_cache.service
